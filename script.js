96a6da6308b7e2dc1e727981647ad489105c78b4
Nicolas Finotti <nicolasfinotti@hotmail.com>
2023-09-14 20:59:55 -0300

    catalogo

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-import PhotoSwipeLightbox from './assets/photoswipe/photoswipe-lightbox.esm.min.js';
+import PhotoSwipeLightbox from "./assets/photoswipe/photoswipe-lightbox.esm.min.js";
+
 
 window.onload = function () {
-  $(document).on('click', '.scroll__link', function (e) {
+  $(document).on("click", ".scroll__link", function (e) {
     e.preventDefault();
-    const href = $(this).attr('href');
-    $('html, body').animate(
+    const href = $(this).attr("href");
+    $("html, body").animate(
       {
         scrollTop: $(href).offset().top - 80,
       },
@@ -12,50 +13,53 @@ window.onload = function () {
     );
   });
 
-  if ($('#navbar-sticky').length > 0) {
+  if ($("#navbar-sticky").length > 0) {
     const firstScrollSpyEl = document.querySelector('[data-bs-spy="scroll"]');
-    firstScrollSpyEl.addEventListener('activate.bs.scrollspy', () => {
-      if (!$('#navbar-sticky li a.active').length > 0) return;
-      const linkOffsetLeft = $('#navbar-sticky li a.active').offset().left - $('#navbar-sticky').offset().left + $('#navbar-sticky').scrollLeft();
-      const linkWidth = $('#navbar-sticky li a.active').width();
+    firstScrollSpyEl.addEventListener("activate.bs.scrollspy", () => {
+      if (!$("#navbar-sticky li a.active").length > 0) return;
+      const linkOffsetLeft =
+        $("#navbar-sticky li a.active").offset().left -
+        $("#navbar-sticky").offset().left +
+        $("#navbar-sticky").scrollLeft();
+      const linkWidth = $("#navbar-sticky li a.active").width();
 
-      $('#scroll__spy--underline').show();
-      $('#scroll__spy--underline').animate({
+      $("#scroll__spy--underline").show();
+      $("#scroll__spy--underline").animate({
         left: linkOffsetLeft,
         width: linkWidth,
       });
 
       if (linkOffsetLeft + linkWidth > $(window).width()) {
-        $('#navbar-sticky').stop().animate({
+        $("#navbar-sticky").stop().animate({
           scrollLeft: linkOffsetLeft,
         });
-      } else if ($('#navbar-sticky li a.active').offset().left < 0) {
-        $('#navbar-sticky')
+      } else if ($("#navbar-sticky li a.active").offset().left < 0) {
+        $("#navbar-sticky")
           .stop()
           .animate({
-            scrollLeft: $('#navbar-sticky li a.active').offset().left,
+            scrollLeft: $("#navbar-sticky li a.active").offset().left,
           });
       }
     });
   }
 
-  if ($('.parallax').length > 0) {
+  if ($(".parallax").length > 0) {
     lax.init();
 
     // Add a driver that we use to control our animations
-    lax.addDriver('scrollY', function () {
+    lax.addDriver("scrollY", function () {
       return window.scrollY;
     });
 
-    const parallaxHeight = $('.parallax').height().toFixed() / 2;
+    const parallaxHeight = $(".parallax").height().toFixed() / 2;
 
     lax.addElements(
-      '.parallax__bg--overlay', // Element selector rule
+      ".parallax__bg--overlay", // Element selector rule
       {
         // Animation data
         scrollY: {
           opacity: [
-            [`elInY+${parallaxHeight}`, 'elOutY'],
+            [`elInY+${parallaxHeight}`, "elOutY"],
             [0, 0.8],
           ],
         },
@@ -63,12 +67,12 @@ window.onload = function () {
     );
 
     lax.addElements(
-      '.parallax__bg img', // Element selector rule
+      ".parallax__bg img", // Element selector rule
       {
         // Animation data
         scrollY: {
           scale: [
-            ['elInY', 'elOutY'],
+            ["elInY", "elOutY"],
             [1, 1.05],
           ],
         },
@@ -76,12 +80,12 @@ window.onload = function () {
     );
 
     lax.addElements(
-      '.parallax__content--text', // Element selector rule
+      ".parallax__content--text", // Element selector rule
       {
         // Animation data
         scrollY: {
           opacity: [
-            ['elInY', 'elInY+300'],
+            ["elInY", "elInY+300"],
             [0, 1],
           ],
         },
@@ -89,12 +93,12 @@ window.onload = function () {
     );
   }
 
-  if ($('#similar__cars--carousel').length > 0) {
-    new Glide('#similar__cars--carousel', {
-      type: 'carousel',
+  if ($("#similar__cars--carousel").length > 0) {
+    new Glide("#similar__cars--carousel", {
+      type: "carousel",
       startAt: 0,
       perView: 3,
-      focusAt: 'center',
+      focusAt: "center",
       breakpoints: {
         1024: {
           perView: 2,
@@ -106,61 +110,62 @@ window.onload = function () {
     }).mount();
   }
 
-  if ($('#gallery-carousel').length > 0) {
-    new Glide('#gallery-carousel', {
-      type: 'carousel',
+  if ($("#gallery-carousel").length > 0) {
+    new Glide("#gallery-carousel", {
+      type: "carousel",
       startAt: 0,
       perView: 1,
-      focusAt: 'center',
+      focusAt: "center",
     }).mount();
   }
 
-  if ($('#technical__data').length > 0) {
-    $('.technical__data--open').on('click', () => {
-      $('#technical__data').addClass('active').css('display', 'block');
+  if ($("#technical__data").length > 0) {
+    $(".technical__data--open").on("click", () => {
+      $("#technical__data").addClass("active").css("display", "block");
     });
 
-    $('.technical__data--close').on('click', () => {
-      $('#technical__data').removeClass('active');
+    $(".technical__data--close").on("click", () => {
+      $("#technical__data").removeClass("active");
     });
   }
 
-  if ($('.view__360--section').length > 0) {
-    $('.view__360--section .iframe__shield button').on('click', function () {
-      $(this).closest('.iframe__shield').hide();
-      console.log('clicked');
+  if ($(".view__360--section").length > 0) {
+    $(".view__360--section .iframe__shield button").on("click", function () {
+      $(this).closest(".iframe__shield").hide();
+      console.log("clicked");
     });
 
-    $('.view__360--section .iframe__wrapper').on('mouseleave', function () {
-      $(this).find('.iframe__shield').show();
+    $(".view__360--section .iframe__wrapper").on("mouseleave", function () {
+      $(this).find(".iframe__shield").show();
     });
   }
 
   const intervalGoogleSelect = setInterval(() => {
-    $('#google__translate select').addClass('form-control');
-    $('#google__translate select').attr('id', 'google__translate--select');
-    if ($('#google__translate select').hasClass('form-control')) clearInterval(intervalGoogleSelect);
+    $("#google__translate select").addClass("form-control");
+    $("#google__translate select").attr("id", "google__translate--select");
+    if ($("#google__translate select").hasClass("form-control"))
+      clearInterval(intervalGoogleSelect);
   }, 500);
 
-  if ($('#gallery').length > 0) {
+  if ($("#gallery").length > 0) {
     const galleryLightbox = new PhotoSwipeLightbox({
-      gallery: '#gallery',
-      children: 'a',
-      pswpModule: () => import('./assets/photoswipe/photoswipe.esm.min.js'),
+      gallery: "#gallery",
+      children: "a",
+      pswpModule: () => import("./assets/photoswipe/photoswipe.esm.min.js"),
     });
     galleryLightbox.init();
   }
 
-  if ($('#btn__send--form').length > 0) {
+  if ($("#btn__send--form").length > 0) {
     grecaptcha.ready(function () {
-      grecaptcha.render('btn__send--form', {
-        sitekey: '6LeXAQ4oAAAAAMj0DhaC7J17S-jDUlPlxlrFB3k7',
+      grecaptcha.render("btn__send--form", {
+        sitekey: "6LeXAQ4oAAAAAMj0DhaC7J17S-jDUlPlxlrFB3k7",
       });
     });
 
-    $('#btn__send--form').on('click', function (e) {
+    $("#btn__send--form").on("click", function (e) {
       e.preventDefault();
-      const form = $(this).closest('form');
+      const form = $(this).closest("form");
 
       if (!form[0].checkValidity()) {
         form[0].reportValidity();
@@ -168,8 +173,8 @@ window.onload = function () {
       }
 
       Swal.fire({
-        title: 'Enviando...',
-        html: 'Aguarde um momento...',
+        title: "Enviando...",
+        html: "Aguarde um momento...",
         allowOutsideClick: false,
         allowEscapeKey: false,
         didOpen: async () => {
@@ -178,31 +183,31 @@ window.onload = function () {
 
           try {
             const result = await $.ajax({
-              type: 'POST',
-              url: './enviar.php',
+              type: "POST",
+              url: "./enviar.php",
               data: form.serializeArray(),
             });
 
             console.log(result);
-            if (result == 'sucesso') {
+            if (result == "sucesso") {
               Swal.fire({
-                title: 'Sucesso',
-                text: 'Sua mensagem foi enviada com sucesso!',
-                icon: 'success',
+                title: "Sucesso",
+                text: "Sua mensagem foi enviada com sucesso!",
+                icon: "success",
                 // timer: 3000,
                 // showProgressBar: true,
-                confirmButtonText: 'Ok',
+                confirmButtonText: "Ok",
               }).then((res) => {
                 location.reload();
               });
             } else {
               Swal.fire({
-                title: 'Erro',
-                text: 'Ocorreu um erro ao enviar a mensagem, tente novamente mais tarde.',
-                icon: 'error',
+                title: "Erro",
+                text: "Ocorreu um erro ao enviar a mensagem, tente novamente mais tarde.",
+                icon: "error",
                 // timer: 3000,
                 // showProgressBar: true,
-                confirmButtonText: 'Ok',
+                confirmButtonText: "Ok",
               }).then((res) => {
                 location.reload();
               });
@@ -210,12 +215,12 @@ window.onload = function () {
           } catch (e) {
             console.log(e);
             Swal.fire({
-              title: 'Erro',
-              text: 'Ocorreu um erro ao enviar a mensagem, tente novamente mais tarde.',
-              icon: 'error',
+              title: "Erro",
+              text: "Ocorreu um erro ao enviar a mensagem, tente novamente mais tarde.",
+              icon: "error",
               // timer: 3000,
               // showProgressBar: true,
-              confirmButtonText: 'Ok',
+              confirmButtonText: "Ok",
             }).then((res) => {
               location.reload();
             });
@@ -224,8 +229,8 @@ window.onload = function () {
       });
     });
 
-    const $input = document.querySelector('.phone-mask');
-    $input.addEventListener('input', handleInput, false);
+    const $input = document.querySelector(".phone-mask");
+    $input.addEventListener("input", handleInput, false);
 
     function handleInput(e) {
       e.target.value = phoneMask(e.target.value);
@@ -233,48 +238,63 @@ window.onload = function () {
 
     function phoneMask(phone) {
       return phone
-        .replace(/\D/g, '')
-        .replace(/^(\d)/, '($1')
-        .replace(/^(\(\d{2})(\d)/, '$1) $2')
-        .replace(/(\d{4})(\d{1,5})/, '$1-$2')
-        .replace(/(-\d{5})\d+?$/, '$1');
+        .replace(/\D/g, "")
+        .replace(/^(\d)/, "($1")
+        .replace(/^(\(\d{2})(\d)/, "$1) $2")
+        .replace(/(\d{4})(\d{1,5})/, "$1-$2")
+        .replace(/(-\d{5})\d+?$/, "$1");
     }
   }
 };
 
 // if ($('.seccion').length > 0) {
 const imagenes = [
-  './public/img01.jpg',
-  './public/img02.jpg',
-  './public/img03.jpg',
-  './public/img04.jpg',
-  './public/img05.jpg',
-  './public/img06.jpg',
-  './public/img07.jpg',
-  './public/img08.jpg',
+  "./public/img01.jpg",
+  "./public/img02.jpg",
+  "./public/img03.jpg",
+  "./public/img04.jpg",
+  "./public/img05.jpg",
+  "./public/img06.jpg",
+  "./public/img07.jpg",
+  "./public/img08.jpg",
 ];
-const imagenWrapper = document.querySelector('.imagenWrapper');
-const imagenPreview = document.getElementById('imagenPreview');
-const range = document.getElementById('imgRange');
+const imagenWrapper = document.querySelector(".imagenWrapper");
+const imagenPreview = document.getElementById("imagenPreview");
+const range = document.getElementById("imgRange");
 const imagenTags = [];
-imagenWrapper.classList.add('cargando'); //Se agrega el loader al iniciar
+imagenWrapper.classList.add("cargando"); //Se agrega el loader al iniciar
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   let imgLoaded = 0; //Acumulador que nos va a servir para saber cuando se cargan todas las imagenes
   imagenes.map((imgSrc, index) => {
-    if (index == 0) imagenPreview.setAttribute('src', imgSrc); //Renderizamos la primer imagen
+    if (index == 0) imagenPreview.setAttribute("src", imgSrc); //Renderizamos la primer imagen
     const imagen = new Image(); //Creamos el elemento imagen. Esto nos sirve para no hacer un nuevo request cada que cambia el valor del range
-    imagen.setAttribute('src', imgSrc);
-    imagen.addEventListener('load', () => {
+    imagen.setAttribute("src", imgSrc);
+    imagen.addEventListener("load", () => {
       imgLoaded++; //Aumentamos el incrementador cuando cada imagen sea cargada
       imagenTags[index] = imagen;
-      if (imgLoaded == imagenes.length) imagenWrapper.classList.remove('cargando'); //Se remueve el loader cuando todas las imagenes estan cargadas
+      if (imgLoaded == imagenes.length)
+        imagenWrapper.classList.remove("cargando"); //Se remueve el loader cuando todas las imagenes estan cargadas
     });
   });
-  range.addEventListener('input', () => {
+  range.addEventListener("input", () => {
     console.log(range.value);
     let val = range.value;
-    imagenPreview.setAttribute('src', imagenTags[val - 1].getAttribute('src')); //Obtenemos el src de las tags previamente creadas y se lo pasamos al preview para que lo renderize
+    imagenPreview.setAttribute("src", imagenTags[val - 1].getAttribute("src")); //Obtenemos el src de las tags previamente creadas y se lo pasamos al preview para que lo renderize
   });
 });
 // }
+
+var $catalogoBody = $(".catalogo-bd").isotope({
+  // options
+  itemSelector: ".ctlg-car",
+  layoutMode: "fitRows"
+})
+  $('.btn-section .button').on('click',function(){
+  $('.btn-section .button') removeClass('.active');
+  $(this).addClass('.active');
+  var value = $(this).attr('data-filter');
+  $catalogoBody.isotope({
+    filter: value
+  })
+});
